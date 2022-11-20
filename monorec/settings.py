4a95f9495226841c05f7b1af975710fe731b8b48
Lukasz <lukabr@op.pl>
2022-11-20 20:04:55 +0100

    Dodałem meta by działał ajax

--- monorec/settings.py
+++ monorec/settings.py
@@ -127,8 +127,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
-# DATABASE_URL = os.getenv("DATABASE_URL")
-#
-# DATABASES = {
-#
-#     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),}
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASES = {
+
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+    'NAME': 'postgresql',}
