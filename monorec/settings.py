1839b93587403acbc5fb7ebc20a483cdf8b8bc01
Lukasz <lukabr@op.pl>
2022-12-26 13:11:45 +0100

    Twoja Receptura

--- monorec/settings.py
+++ monorec/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASES = {
-
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
-    }
+# DATABASES = {
+#
+#     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+#     }
