4c57fe931bc6666f05cc923921a398a0b5ac0d0b
lukab <lukabr@op.pl>
2025-12-27 21:43:56 +0100

    DEBUG 2

--- monorec/settings.py
+++ monorec/settings.py
@@ -27,7 +27,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = str(os.environ.get('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG  =  str(os.environ.get('DEBUG'))
-DEBUG= os.getenv("DEBUG")
+#DEBUG= os.getenv("DEBUG")
+DEBUG = False
+
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app','https://twoja-receptura.pl']
 
